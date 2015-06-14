d54b4f2c9b90aa14edb1e27b3d5a72553412aeb0
Piotr Zduniak <piotr@zduniak.net>
2015-06-14 17:48:17 +0200

    Revert "Support for sentry_log"
    This reverts commit 9cd5ea6bf5af6a9ead608c5bba153e7f5c259b14.

--- interfaces.go
+++ interfaces.go
@@ -45,33 +45,3 @@ type Query struct {
 }
 
 func (q *Query) Class() string { return "sentry.interfaces.Query" }
-
-type Log struct {
-	CommitID string      `json:"commit_id"`
-	Version  string      `json:"version"`
-	Assets   []string    `json:"assets"`
-	Entries  []*LogEntry `json:"entries"`
-}
-
-func (l *Log) Class() { return "sentry_log.Log" }
-
-type LogEntry struct {
-	Date       int64       `json:"date"`
-	Type       string      `json:"type"`
-	Message    string      `json:"message"`
-	Objects    interface{} `json:"objects"`
-	Stacktrace []*LogFrame `json:"stacktrace"`
-}
-
-type LogFrame struct {
-	Filename    string   `json:"filename"`
-	Name        string   `json:"name"`
-	LineNo      int      `json:"line_no"`
-	ColNo       int      `json:"col_no"`
-	InApp       bool     `json:"in_app"`
-	ContextPre  []string `json:"context_pre,omitempty"`
-	ContextLine string   `json:"context_line,omitempty"`
-	ContextPost []string `json:"context_post,omitempty"`
-	AbsPath     string   `json:"abs_path"`
-	StartLineNo int      `json:"start_line_no"`
-}
